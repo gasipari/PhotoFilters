185e10ada537b3c090cf251eb856859afdcd4aa2
Marius Muk <riamarius@gmail.com>
2015-05-05 09:54:23 -0400

    initial commit

--- PhotoFilters/ViewController.swift
+++ PhotoFilters/ViewController.swift
@@ -9,7 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    // place holder for render the filtered image
+    let context = CIContext(options: nil)
+    
+    @IBAction func applyFilter(sender: AnyObject) {
+        
+        // create image to filter
+        let inputImage = CIImage(image:photoImageView.image)
+        
+        // create a random color to pass to the filter
+        let randomColor = [kCIInputAngleKey: (Double(arc4random_uniform(314)) / 100)]
+        
+        // apply filter to image
+        let filteredImage = inputImage.imageByApplyingFilter("CIHueAdjust", withInputParameters: randomColor)
+        
+        // render filtered image
+        let renderedImage = context.createCGImage(filteredImage, fromRect: filteredImage.extent())
+        
+        //reflect the change back in the view
+        photoImageView.image = UIImage(CGImage: renderedImage)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
